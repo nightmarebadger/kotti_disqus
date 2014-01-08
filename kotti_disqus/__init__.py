367e7f6e8c8c60d58d32f2003dab8e34a1fdd811
Natan Žabkar <natan.zabkar@gmail.com>
2014-01-08 21:59:44 +0100

    Remove some stuff that is not needed

--- kotti_disqus/__init__.py
+++ kotti_disqus/__init__.py
@@ -7,10 +7,7 @@ def kotti_configure(settings):
 
     settings['pyramid.includes'] += ' kotti_disqus'
 
-    settings['kotti.available_types'] += ' kotti_disqus.resources.content_type'
-
 
 def includeme(config):
-
     config.add_translation_dirs('kotti_disqus:locale')
     config.scan(__name__)
--- kotti_disqus/views.py
+++ kotti_disqus/views.py
@@ -1,66 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from colander import SchemaNode
-from colander import String
-from kotti.views.edit.content import ContentSchema
-from kotti.views.form import AddFormView
-from kotti.views.form import EditFormView
 from pyramid.view import view_config
-from pyramid.view import view_defaults
-
-from kotti_disqus import _
-from kotti_disqus.resources import content_type
-from kotti_disqus.fanstatic import kotti_disqus
 
 
-class content_typeSchema(ContentSchema):
-    """Schema for add / edit forms of content_type"""
-
-    example_attribute = SchemaNode(
-        String(),
-        title=_(u'Example Attribute'),
-        missing=u"",
-        )
 
 
-@view_config(name=content_type.type_info.add_view,
-             permission='add',
-             renderer='kotti:templates/edit/node.pt')
-class content_typeAddForm(AddFormView):
-
-    schema_factory = content_typeSchema
-    add = content_type
-    item_type = _(u"content_type")
-
-
-@view_config(name='edit',
-             context=content_type,
-             permission='edit',
-             renderer='kotti:templates/edit/node.pt')
-class content_typeEditForm(EditFormView):
-
-    schema_factory = content_typeSchema
-
-
-@view_defaults(context=content_type, permission='view')
-class content_typeView(object):
-    """View(s) for content_type"""
-
-    def __init__(self, context, request):
-
-        self.context = context
-        self.request = request
-
-    @view_config(name='view',
-                 renderer='kotti_disqus:templates/content_type.pt')
-    def view(self):
-
-        kotti_disqus.need()
-
-        return {}
-
-    @view_config(name='alternative-view',
-                 renderer='kotti_disqus:templates/content_type-alternative.pt')
-    def alternative_view(self):
-
-        return {}
