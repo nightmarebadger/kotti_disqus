906ad716da09b19482e0e51db12085a877ac1d10
Natan Žabkar <natan.zabkar@gmail.com>
2014-01-12 12:00:54 +0100

    Use pytest.raises in tests

--- kotti_disqus/tests/test_views.py
+++ kotti_disqus/tests/test_views.py
@@ -4,6 +4,7 @@ from kotti.resources import get_root
 from kotti.resources import Image
 from kotti_settings.util import set_setting
 from pyramid.exceptions import PredicateMismatch
+import pytest
 
 from kotti_disqus import kotti_configure
 from kotti_disqus.views import disqus_comments_view
@@ -19,6 +20,7 @@ def test_views(db_session, dummy_request):
     kotti_configure(get_settings())
     set_setting('kotti_disqus-disqus_available_types',
                 ['kotti.resources.Document'])
+
     # Is shown on Document with the default URL if we do not set it
     set_setting('kotti_disqus-disqus_shortname', 'test_shortname')
     set_setting('kotti_disqus-disqus_base_url', '')
@@ -37,9 +39,5 @@ def test_views(db_session, dummy_request):
         }
 
     # Is not shown if we aren't on a Document object, raises PredicateMismatch
-    try:
+    with pytest.raises(PredicateMismatch):
         disqus_comments_view(root['test-image'], dummy_request)
-        # Fail if we didn't raise a PredicateMismatch
-        assert True is False  # pragma: no cover
-    except PredicateMismatch:
-        pass
